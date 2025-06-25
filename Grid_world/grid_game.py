7b9a0653c21220816874faa860f267aa8615f95b
Gowravsharma <gowravsharma8849@gmail.com>
2025-06-25 21:32:07 +0530

    Packed inside classes

--- Grid_world/grid_game.py
+++ Grid_world/grid_game.py
@@ -1,63 +1,72 @@
 import numpy as np
 
+class Grid_World:
+  def __init__(self, rows, cols, moves = ['up','down','left','right'], transition_prob = {} ):
+    self.rows = rows
+    self.cols = cols
+    self.moves = moves
+    self.transition_prob = transition_prob
+    self.total_states = rows*cols
+    self.states = np.arange(0,self.total_states,1) # [0,1,2,..., termial_state]
+    self.terminal_state = self.total_states - 1
+    
+    if not self.transition_prob:
+      for i in range(self.total_states):
+        transition_prob[i] = {}
+        valid_actions = []
+        for j in moves:
+          if (j == 'up' and i - cols < 0) or \
+            (j == 'down' and i + cols >= self.total_states) or \
+            (j == 'right' and (i + 1) % cols == 0) or \
+            (j == 'left' and i % cols == 0):
+            transition_prob[i][j] = 0
+            continue
+          valid_actions.append(j)
+
+        prob = 1 / len(valid_actions)
+        for j in valid_actions:
+          transition_prob[i][j] = prob
+  def sample_next_action(self, from_state):
+    actions_probs = [(a, p) for a, p in self.transition_prob[from_state].items() if p > 0]
+    actions, probs = zip(*actions_probs)
+    return np.random.choice(actions, p=probs)
+      
+  def next_state(self, current_state, action):
+    if(action == 'up'):
+      current_state = current_state - self.cols
+    if(action == 'down'):
+      current_state = current_state + self.cols
+    if(action == 'left'):
+      current_state = current_state - 1
+    if(action == 'right'):
+      current_state = current_state + 1
+    return current_state
+
+  def is_terminal(self, state):
+    return state == self.terminal_state
+
+  def play_game(self, start_state):
+    current_state = start_state
+    sampled_states = []
+    while not self.is_terminal(current_state):
+      action = self.sample_next_action(current_state)
+      sampled_states.append([current_state,action])
+      current_state = self.next_state(current_state, action)
+    return sampled_states
+
 #Grid dimensions
-rows,cols = 5,5
-total_states = rows*cols
-states = np.arange(0,total_states,1) # [0,1,2,..., termial_state]
-terminal_state = total_states-1
-moves = ['up','down','left','right']
-
-transition_prob = {} #policy
-
-for i in range(total_states):
-  transition_prob[i] = {}
-  valid_actions = []
-  for j in moves:
-    if (j == 'up' and i - cols < 0) or \
-      (j == 'down' and i + cols >= total_states) or \
-      (j == 'right' and (i + 1) % cols == 0) or \
-      (j == 'left' and i % cols == 0):
-      transition_prob[i][j] = 0
-      continue
-    valid_actions.append(j)
-
-  prob = 1 / len(valid_actions)
-  for j in valid_actions:
-    transition_prob[i][j] = prob
-
-
-def sample_next_action(from_state):
-  actions_probs = [(a, p) for a, p in transition_prob[from_state].items() if p > 0]
-  actions, probs = zip(*actions_probs)
-  return np.random.choice(actions, p=probs)
-
+#rows,cols = 5,5
+#total_states = rows*cols
+#states = np.arange(0,total_states,1) # [0,1,2,..., termial_state]
+#terminal_state = total_states-1
+#moves = ['up','down','left','right']
+#transition_prob = {} #policy
 #print(sample_next_action(14)) for debuging
 #for i in range(16):
 #  print(sample_next_action(i))
 
-def next_state(current_state, action):
-  if(action == 'up'):
-    current_state = current_state - cols
-  if(action == 'down'):
-    current_state = current_state + cols
-  if(action == 'left'):
-    current_state = current_state - 1
-  if(action == 'right'):
-    current_state = current_state + 1
-  return current_state
-
-def is_terminal(state):
-  return state == rows*cols - 1
-
-def play_game(start_state):
-  current_state = start_state
-  sampled_states = []
-  while not is_terminal(current_state):
-    action = sample_next_action(current_state)
-    sampled_states.append([current_state,action])
-    current_state = next_state(current_state, action)
-  return sampled_states
-
 
 if __name__ == '__main__':
-  print(play_game(0), play_game(0).shape())  
+  g1 = Grid_World(5,5)
+  trajectory = g1.play_game(0)
+  print(trajectory, '\nLenght of trajectory: ',len(trajectory))  
--- Grid_world/grid_mc.py
+++ Grid_world/grid_mc.py
@@ -1,11 +1,13 @@
 import grid_game as game
 import numpy as np
 
+game1 = game.Grid_World(6,6)
+
 discount_factor = 1
 epsilon = 0.1
 step_size = 0.01
 num_episodes = 10000
-num_states = game.total_states
+num_states = game1.total_states
 
 
 state_value_ev = np.zeros(num_states)
@@ -16,81 +18,88 @@ state_reward[num_states-1] = 2
 
 print(f'number of states {num_states}')
 
-# Q_val Dictionary
-Q = {
-  s: {a: 0.0 for a in game.moves
-      if not ((a == 'up' and s - game.rows < 0) or
-              (a == 'down' and s + game.rows >= num_states) or
-              (a == 'right' and (s + 1) % game.rows == 0) or
-              (a == 'left' and s % game.rows == 0))}
-  for s in range(num_states)
-}
+class Monte_Carlo:
+  def __init__(self,discount_factor = 1, step_size = 0.01,num_states = game1.total_states,Q = {},):
+    self.discount_factor = discount_factor
+    self.step_size = step_size
+    self.num_states = num_states
+    self.Q = Q # Q_val Dictionary
 
+    if not self.Q:
+      self.Q = {
+            s: {a: 0.0 for a in game1.moves
+                if not ((a == 'up' and s - game1.rows < 0) or
+                        (a == 'down' and s + game1.rows >= num_states) or
+                        (a == 'right' and (s + 1) % game1.rows == 0) or
+                        (a == 'left' and s % game1.rows == 0))}
+            for s in range(num_states)
+          }
 
+  def every_visit_mc(num_episodes):
+    for i in range(num_episodes):
+      initial_state = np.random.choice(game1.states)
+      episode = game1.play_game(initial_state)
 
-def every_visit_mc(num_episodes):
-  for i in range(num_episodes):
-    initial_state = np.random.choice(game.states)
-    episode = game.play_game(initial_state)
+      G = 0
+      visited_states = set()
+      state = None
+      for s in reversed(range(len(episode))):
+        state = episode[s][0]
+        reward = state_reward[state]
 
-    G = 0
-    visited_states = set()
-    state = None
-    for s in reversed(range(len(episode))):
-      state = episode[s][0]
-      reward = state_reward[state]
+        G = discount_factor*G + reward
 
-      G = discount_factor*G + reward
+        state_value_ev[state]+= step_size*(G - state_value_ev[state])
+      #print(state)
 
-      state_value_ev[state]+= step_size*(G - state_value_ev[state])
-    #print(state)
+  def first_visit_mc(num_episodes):
+    for episodes_num in range(num_episodes):
+      initial_state = np.random.choice(game1.states)
+      episodes = game1.play_game(initial_state) # returns [[state, action], ...]
+
+      G = 0
+      visited = set()
+      state_indices = {}
+
+      for idx,(state,action) in enumerate(episodes):
+        if state not in state_indices:
+          state_indices[state] = idx
+
+      for state, first_idx in state_indices.items():
+        G = 0
+        for t in reversed(range(first_idx, len(episodes))):
+          s = episodes[t][0]
+          G = discount_factor*G + state_reward[s]
+        state_value_fv[state] += step_size*(G - state_value_fv[state])
+
+  def epsilon_greedy_action(self, state, epsilon = 0.1):
+    if np.random.rand() < epsilon:
+      return game1.sample_next_action
+    else:
+      q_values = self.Q[state]
+      return max(q_values, key= q_values.get)
 
 #if __name__ == '__main__':
 #  every_visit_mc(num_episodes)
 #  print('Every Visit Monte carlo:' ,'\n', state_value_ev.reshape((4,4)))
 
-def first_visit_mc(num_episodes):
-  for episodes_num in range(num_episodes):
-    initial_state = np.random.choice(game.states)
-    episodes = game.play_game(initial_state) # returns [[state, action], ...]
-
-    G = 0
-    visited = set()
-    state_indices = {}
-
-    for idx,(state,action) in enumerate(episodes):
-      if state not in state_indices:
-        state_indices[state] = idx
-
-    for state, first_idx in state_indices.items():
-      G = 0
-      for t in reversed(range(first_idx, len(episodes))):
-        s = episodes[t][0]
-        G = discount_factor*G + state_reward[s]
-      state_value_fv[state] += step_size*(G - state_value_fv[state])
 
 #if __name__ == '__main__':
 #  first_visit_mc(num_episodes)
 #  print('First Visit Monte carlo:' ,'\n', state_value_fv.reshape((4,4)))
 
 # Epsilon Greedy policy
-def epsilon_greedy_action(Q,state):
-
-  if np.random.rand() < epsilon:
-    return game.sample_next_action
-  else:
-    q_values = Q[state]
-    return max(q_values, key= q_values.get)
   
 
-def update_transition_policy(Q, epsilon = 0.1):
+def update_transition_policy(self,epsilon = 0.1):
+  Q = self.Q
   new_policy = {}
   
   for state in range(num_states):
-    valid_actions = [a for a,p in game.transition_prob[state].items() if p > 0]
+    valid_actions = [a for a,p in game1.transition_prob[state].items() if p > 0]
 
     if not valid_actions:
-      new_policy[state] = {a:0 for a in game.moves}
+      new_policy[state] = {a:0 for a in game1.moves}
       continue
     
     #finding greedy action among valid action comparing Q value
@@ -105,19 +114,20 @@ def update_transition_policy(Q, epsilon = 0.1):
       else:
         probs[a] = epsilon/num_actions
 
-    new_policy[state] = {a: probs.get(a, 0.0) for a in game.moves}
+    new_policy[state] = {a: probs.get(a, 0.0) for a in game1.moves}
   
   return new_policy
 
-def every_visite_mc_control(num_episodes):
+def every_visite_mc_control(self,num_episodes):
+  Q = self.Q
   for episode in range(num_episodes):
     episode_trace = []
-    state = np.random.choice(game.states)
-    initial_action = epsilon_greedy_action(Q,state)
+    state = np.random.choice(game1.states)
+    initial_action = self.epsilon_greedy_action(Q,state)
 
-    next_state = game.next_state(state, initial_action)
+    next_state = game1.next_state(state, initial_action)
     
-    episodes = game.play_game(next_state)
+    episodes = game1.play_game(next_state)
     G = 0
     state = None
     visited = set()
@@ -131,15 +141,14 @@ def every_visite_mc_control(num_episodes):
         #print(s,a)
         Q[s][a] += step_size*(G - Q[s][a])
     
-    game.transition_prob = update_transition_policy(Q)
+    game1.transition_prob = update_transition_policy(Q)
 
-every_visite_mc_control(num_episodes)
 
-def print_path_gridworld(initial_state, rows, cols):
-  game.rows , game.cols = rows, cols
+def print_path_gridworld(self, initial_state, rows, cols):
+  game1.rows , game1.cols = rows, cols
 
   # path
-  agent_trajectory = game.play_game(initial_state)
+  agent_trajectory = game1.play_game(initial_state)
   
   action_arrows = {'up':'↑','down':'↓','left': '←', 'right':'→'}
   
