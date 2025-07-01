26c8f47e98b9a554cb09edbec9f3e1c9e4c02a3e
Gowravsharma <gowravsharma8849@gmail.com>
2025-07-01 21:13:19 +0530

    commit

--- Tic_Tac_Toe/tic_tac_toe_game.py
+++ Tic_Tac_Toe/tic_tac_toe_game.py
@@ -5,24 +5,32 @@ blank_places = places
 filled_place = []
 
 class tic_tac_toe:
-  def __init__():
-  def update_blank_places(place):
+  filled_places = None
+  blank_places = None
+  places = [1,2,3,4,5,6,7,8,9]
+  
+  def __init__(self, blank_places = None, filled_places = None, trajectory = None):
+    self.places = [1,2,3,4,5,6,7,8,9]
+    self.blank_places = blank_places if blank_places is not None else self.places
+    self.filled_places = filled_places if filled_places is not None else []
+    self.trajectory = trajectory
+
+  def update_blank_places(self, place):
       list = []
       for _ in blank_places:
         if _ != place:
           list.append(_)
-      blank_places = list
+      self.blank_places = list
 
-  def sample_next_place(blank_places):
+  def sample_next_place(self,blank_places):
     num_blank_places = len(blank_places)
     prob = [1/num_blank_places]*num_blank_places
     next_place = np.random.choice(blank_places, p = prob)
-    
-    update_blank_places(next_place)
+    self.update_blank_places(next_place)
 
     return next_place
     
-  def is_terminal_state(state):
+  def is_terminal_state(self, state):
     filled_place , blank_place = state
     terminal_list = [[1,2,3],[4,5,6],[7,8,9],
                     [1,4,7],[2,5,8],[3,6,9],
@@ -33,30 +41,30 @@ class tic_tac_toe:
     
     return False
 
-  def update_state(place):
-    filled_place.append(place)
+  def update_state(self, place):
+    self.filled_place.append(place)
     curret_state = (filled_place, blank_places)
     return curret_state
 
-  def play_game(start_place = None):
+  def play_game(self, start_place = None):
     trajectory = []
-    intial_place = sample_next_place(blank_places)
+    intial_place = self.sample_next_place(blank_places)
     filled_place.append(intial_place)
     if place is not None:
       filled_place.append(start_place)
-      update_blank_places(start_place)
+      self.update_blank_places(start_place)
 
     else:
-        intial_place = sample_next_place(blank_places)
+        intial_place = self.sample_next_place(blank_places)
         filled_place.append(intial_place)
     
     current_state = (filled_place, blank_places)
 
-    while not is_terminal_state(current_state):
-      place = sample_next_place(blank_places)
+    while not self.is_terminal_state(current_state):
+      place = self.sample_next_place(blank_places)
       trajectory.append(place)
-      current_state = update_state(place)
+      current_state = self.update_state(place)
 
     return trajectory
 
-print(play_game())
+print(play_game()) 
