e3bce3f8b275e7c580b3867c15298869b75ec526
Gowravsharma <gowravsharma8849@gmail.com>
2025-07-10 14:19:12 +0530

    completed TD(SARSA) but have some bugs in Q val

--- Tic_Tac_Toe/TD.py
+++ Tic_Tac_Toe/TD.py
@@ -1,22 +1,42 @@
 from test import Player, Game
 import numpy as np
 
-player1 = Player('player1')
-player2 = Player('player2')
-
 class TD(Game):
-  def __init__(self, Q = {}):
-    super.__init__(player1, player2)
-  def sample_epsilon_greedy_action(self, player, state, epsilon = 0.1):
-    if not player.Q[state]:
-      num_actions = len(self.blank_places)
-      prob = 1/num_actions if num_actions != 0 else 0
-      player.Q[state] = {act: prob for act in self.blank_places}
-    else:
-      if np.random.rand() < epsilon:
-        return player.move(self.blank_places)
-      else:
-        q_values = self.Q[state]
-        return max(q_values, key = q_values.get)
+  def __init__(self, player1, player2,step_size, discount_factor, epsilon = None):
+    super().__init__(player1, player2)
+    self.player1 = player1
+    self.player2 = player2
+    self.step_size = step_size
+    self.discount_factor = discount_factor
+    self.epsilon = epsilon if epsilon is not None else 0.1
   
-  
+  def TD_control(self, num_episodes):
+    for episode in range(num_episodes):
+      episode_trace = []
+
+      episodes = self.play()
+      player1 = self.players[self.start_player]
+      player2 = self.players[1-self.start_player]
+      ep1 = episodes[self.start_player]
+      ep2 = episodes[1-self.start_player]
+      G = 0
+
+      for s in range(len(ep1) - 1):
+        state_t,action_t,reward_t = ep1[s]
+        state_tp1 , action_tp1, reward_tp1 = ep1[s+1]
+
+        if action_t == None or action_tp1 is None:
+          continue
+        
+        q_predict = player1.Q[state_t][action_t]
+        q_target = reward_tp1 + self.discount_factor * player1.Q[state_tp1][action_tp1]
+        player1.Q[state_t][action_t] += self.step_size * (q_target - q_predict)
+
+if __name__ == '__main__':
+  player1 = Player(name = 'player1')
+  player2 = Player(name = 'player2')
+
+  td1 = TD(player1, player2,step_size = 0.01, discount_factor = 0.1, epsilon = 0.1)
+  td1.TD_control(1000)
+  td1.play()
+  print(td1.filled_places_players)
--- Tic_Tac_Toe/test.py
+++ Tic_Tac_Toe/test.py
@@ -5,19 +5,20 @@ class Player:
     self.name = name
     self.filled_place = []
     self.trajectory = []
-    self.Q_values = {}
+    self.win = False
+    self.Q = {}
 
-  def move(self, blank_places, state = None):
+  def move(self, blank_places):
     if not blank_places:
       return None
     next_move = np.random.choice(blank_places)
     self.filled_place.append(next_move)
     self.trajectory.append(next_move)
     return next_move
-  
 
 class Game:
   def __init__(self, player1, player2):
+    self.start_player = None
     self.state = []
     self.players = [player1, player2]
     self.blank_places = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -33,23 +34,51 @@ class Game:
           return True
     return False
   
+  def sample_epsilon_greedy_action(self, player, state, epsilon = 0.1):
+    if not player.Q[state]:
+      num_actions = len(self.blank_places)
+      prob = 1/num_actions if num_actions != 0 else 0
+      player.Q[state] = {act: prob for act in self.blank_places}
+      return player.move(self.blank_places)
+    else:
+      if np.random.rand() < epsilon:
+        return player.move(self.blank_places)
+      else:
+        q_values = player.Q[state]
+        return max(q_values, key = q_values.get)
+      
   def play(self):
+    sampled_states = [[],[]]
+    self.turn = np.random.choice([0,1])
+    self.start_player = self.turn
     while self.blank_places:
+      s = []
       current_player = self.players[self.turn]
-      move = current_player.move(self.blank_places)
+      state = self.filled_places_players
+      s.append(state) # s = [state]
+      #sampled_states[self.turn].append[state] # inserting state in trajectory i.e., sampled_states[turn][0]
+      move = self.sample_epsilon_greedy_action(current_player, state)
+      s.append(move) # s = [state, move]
+      #sampled_states[self.turn].append[move] # inserting action after the state i.e., sampled_states[turn][1]
       self.filled_places_players[self.turn].append(move)
+      
       if move is None:
-        print("No moves left!")
+        #print("No moves left!")
         break
       self.blank_places.remove(move)
 
-      print(f"{current_player.name} move {move}")
+      #print(f"{current_player.name} move {move}")
 
       if self.is_terminal_state(current_player.filled_place):
-        print(f"{current_player.name} wins")
+        current_player.win = True
+        s.append(10) # s = [state, move, reward(10)]
+        #print(f"{current_player.name} wins")
         return
-
+      
+      s.append(-1) # s = [state, move, reward(-1)]
+      sampled_states[self.turn].append(s) 
       self.turn = 1 - self.turn  # switch turn
+      return sampled_states
 
     print("Tie")
 
